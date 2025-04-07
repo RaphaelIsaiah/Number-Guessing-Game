862c286fc26dd5f79396eda74280396f04cf4023
RaphaelIsaiah <rafvie12@gmail.com>
2025-04-07 16:34:38 +0100

    Initial commit: Add initial scripts for number guessing game with database integration

---
+++ commands.sh
@@ -0,0 +1,11 @@
+# terminal bash commands
+
+psql --username=freecodecamp --dbname=postgres
+
+# After creating database
+mkdir number_guessing_game
+touch number_guess.sh
+chmod +x number_guess.sh
+git init
+git add .
+git commit -m "Initial commit"
---
+++ commands.sql
@@ -0,0 +1,15 @@
+-- psql commands to create the database
+
+CREATE DATABASE number_guess;
+\c number_guess
+
+CREATE TABLE users();
+ALTER TABLE users add COLUMN user_id SERIAL PRIMARY KEY;
+ALTER TABLE users add COLUMN username VARCHAR(22) NOT NULL UNIQUE;
+
+CREATE TABLE games();
+ALTER TABLE games ADD COLUMN game_id SERIAL PRIMARY KEY;
+ALTER TABLE games ADD COLUMN user_id INT NOT NULL;
+ALTER TABLE games ADD CONSTRAINT fk_users_games FOREIGN KEY (user_id) REFERENCES users(user_id);
+ALTER TABLE games ADD COLUMN guesses INT NOT NULL DEFAULT(0);
+
